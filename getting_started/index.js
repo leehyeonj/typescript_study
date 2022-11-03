009c8e5046759df656ffc3b9dc6935ff3f19a529
leehyeonj <billaa@naver.com>
2022-11-03 15:57:34 +0900

    initial commit

---
+++ getting_started/index.js
@@ -0,0 +1,4 @@
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20); //js파일로 컴파일이 필요함
---
+++ getting_started/index.ts
@@ -0,0 +1,4 @@
+function sum(a: number, b: number): number {
+    return a + b; 
+}
+sum(10, 20);//js파일로 컴파일이 필요함
